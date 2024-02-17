2b8e6593a4048c88afbff146eedd1f420746399e
Liwgar <wilenri341@hotmail.com>
2024-02-17 18:22:34 -0500

    Instalacion y prueba de rutas con Express.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-console.log('My App');
+const express = require('express');
+
+const app = express();
+
+const port = 3000;
+
+app.get('/', (request, response) => {
+  response.send('Hello my server in express');
+});
+
+app.get('/nueva-ruta', (request, response) => {
+  response.send('Hello, I am a new route');
+});
+
+app.get('/products', (request, response) => {
+  response.json({
+    name: 'Product 1',
+    price: 10
+  });
+});
+
+app.get('/categories', (request, response) => {
+  response.json({
+    name: 'Electronics'
+  });
+});
+
+app.listen(port, () => {
+  console.log(`La API está escuchando en el puerto ${port}`);
+});
+
+
+
+
