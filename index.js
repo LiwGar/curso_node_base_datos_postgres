e4441dc972ed3859a0506b1021c2fd6f0625441c
Liwgar <wilenri341@hotmail.com>
2024-02-17 19:16:39 -0500

    Probando diferentes  endpoints con GET.

--- index.js
+++ index.js
@@ -13,15 +13,62 @@ app.get('/nueva-ruta', (request, response) => {
 });
 
 app.get('/products', (request, response) => {
+  response.json([
+    {
+      name: 'Product 1',
+      price: 10
+    },
+    {
+      name: 'Product 2',
+      price: 10
+    }
+  ]);
+});
+
+app.get('/products/:productId', (request, response) => {
+  const {productId} = request.params;
   response.json({
-    name: 'Product 1',
+    productId,
+    name: 'Product 2',
     price: 10
   });
 });
 
 app.get('/categories', (request, response) => {
+  response.json([
+    {
+      name: 'Electronic',
+      imagen: 'imagen',
+    },
+    {
+      name: 'Toys',
+      imagen: 'imagen',
+    },
+    {
+      name: 'Clothes',
+      imagen: 'imagen',
+    },
+    {
+      name: 'Books',
+      imagen: 'imagen',
+    }
+  ]);
+});
+
+app.get('/categories/:categoryId', (request, response) => {
+  const {categoryId} = request.params;
   response.json({
-    name: 'Electronics'
+    categoryId,
+    name: 'Product',
+    imagen: 'imagen',
+  });
+});
+
+app.get('/categories/:categoryId/products/:productId', (request, response) => {
+  const {categoryId, productId} = request.params;
+  response.json({
+    categoryId,
+    productId,
   });
 });
 
