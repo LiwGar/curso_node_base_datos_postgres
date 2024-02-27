1ec97e1fbdca0cb34af7b525314f3ec67db096ba
Liwgar <wilenri341@hotmail.com>
2024-02-27 12:26:34 -0500

    Aplicacion de filtros con operadores. product.schema, product.service.

--- api/schemas/product.schema.js
+++ api/schemas/product.schema.js
@@ -1,11 +1,14 @@
 const Joi = require('joi');
 
 const id = Joi.number().integer();
-const name = Joi.string().min(3).max(25);
-const price = Joi.number().integer().min(10);
+const name = Joi.string().min(3).max(32);
+const price = Joi.number().integer().min(5);
 const description = Joi.string().min(10);
 const image = Joi.string().uri();
 const categoryId = Joi.number().integer();
+
+const price_min = Joi.number().integer();
+const price_max = Joi.number().integer();
 
 const limit = Joi.number().integer();
 const offset = Joi.number().integer();
@@ -32,7 +35,13 @@ const updateProductSchema = Joi.object({
 
 const queryProductSchema = Joi.object({
   limit,
-  offset
+  offset,
+  price,
+  price_min,
+  price_max: price_max.when('price_min', {
+    is: Joi.number().integer(),
+    then: Joi.required(),
+  }) 
 });
 
 module.exports = { createProductSchema, getProductSchema, updateProductSchema, queryProductSchema };
