0016f41564d1465a7850334a62ec3d456dd50dd3
Liwgar <wilenri341@hotmail.com>
2024-02-28 18:40:03 -0500

    cambios, sin parseInt en config.

--- config/config.js
+++ config/config.js
@@ -3,12 +3,12 @@ require('dotenv').config();
 const config = {
   env: (process.env.NODE_ENV || 'dev').toLowerCase(),
   isProd: process.env.NODE_ENV === 'production',
-  port: parseInt(process.env.PORT, 10) || 3000,
+  port: (process.env.PORT, 10) || 3000,
   dbUser: process.env.DB_USER,
   dbPassword: process.env.DB_PASSWORD,
   dbHost: process.env.DB_HOST,
   dbName: process.env.DB_NAME,
-  dbPort: parseInt(process.env.DB_PORT, 10) || 5432,
+  dbPort: (process.env.DB_PORT, 10) || 5432,
   dbUrl: process.env.POSTGRES_URL,
 };
 
