dd625a7016b0d86c06d741b5ceb6bac82a663181
Liwgar <wilenri341@hotmail.com>
2024-02-28 11:32:32 -0500

    Configurando variables de entorno.

--- config/config.js
+++ config/config.js
@@ -1,13 +1,13 @@
 require('dotenv').config();
 
 const config = {
-  env: process.env.NODE_ENV || 'dev',
-  port: process.env.PORT || 3000,
+  env: (process.env.NODE_ENV || 'dev').toLowerCase(),
+  port: parseInt(process.env.PORT, 10) || 3000,
   dbUser: process.env.DB_USER,
   dbPassword: process.env.DB_PASSWORD,
   dbHost: process.env.DB_HOST,
   dbName: process.env.DB_NAME,
-  dbPort: process.env.DB_PORT,
-}
+  dbPort: parseInt(process.env.DB_PORT, 10) || 5432,
+};
 
-module.exports = { config }
+module.exports = { config };
