f89b6bc9a844f17d53bd08082dcbb7b3614dad2b
Liwgar <wilenri341@hotmail.com>
2024-02-28 12:58:25 -0500

    Configurando variables env

--- config/config.js
+++ config/config.js
@@ -2,12 +2,14 @@ require('dotenv').config();
 
 const config = {
   env: (process.env.NODE_ENV || 'dev').toLowerCase(),
+  isProd: process.env.NODE_ENV === 'production',
   port: parseInt(process.env.PORT, 10) || 3000,
   dbUser: process.env.DB_USER,
   dbPassword: process.env.DB_PASSWORD,
   dbHost: process.env.DB_HOST,
   dbName: process.env.DB_NAME,
   dbPort: parseInt(process.env.DB_PORT, 10) || 5432,
+  dbUrl: process.env.DATABASE_URL,
 };
 
 module.exports = { config };
