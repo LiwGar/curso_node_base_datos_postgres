038b58e7cd0f1a0a1af79b5e763ab25e20bb5e00
Liwgar <wilenri341@hotmail.com>
2024-02-28 19:59:55 -0500

    Variables

--- config/config.js
+++ config/config.js
@@ -3,13 +3,13 @@ require('dotenv').config();
 const config = {
   env: (process.env.NODE_ENV || 'dev').toLowerCase(),
   isProd: process.env.NODE_ENV === 'production',
-  port: (process.env.PORT, 10) || 3000,
+  port: parseInt(process.env.PORT, 10) || 3000,
   dbUser: process.env.DB_USER,
   dbPassword: process.env.DB_PASSWORD,
   dbHost: process.env.DB_HOST,
   dbName: process.env.DB_NAME,
-  dbPort: (process.env.DB_PORT, 10) || 5432,
-  dbUrl: process.env.POSTGRES_URL,
+  dbPort: parseInt(process.env.DB_PORT, 10) || 5432,
+  dbUrl: process.env.POSTGRES_URL || '',
 };
 
 module.exports = { config };
